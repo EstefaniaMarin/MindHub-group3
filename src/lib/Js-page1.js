a6761254ee88fe4361451e3d307f22cf49dac070
ROBERTG96 <71914244+ROBERTG96@users.noreply.github.com>
2023-03-23 15:19:42 -0500

    TemplateCardHome

--- src/lib/Js-page1.js
+++ src/lib/Js-page1.js
@@ -1,223 +1,207 @@
 const ArrayData = {
-  fechaActual: "2022-01-01",
-  eventos: [
-    {
-      id: 1,
-      image: "https://loremflickr.com/300/240/conciert?ramdom=1",
-      name: "Collectivities Party",
-      date: "2021-12-12",
-      description:
-        "Enjoy your favourite dishes, from different countries, in a unique event for the whole family.",
-      category: "Food Fair",
-      place: "Room A",
-      capacity: 45000,
-      assistance: 42756,
-      price: 5,
-    },
-    {
-      id: 2,
-      image: "https://loremflickr.com/310/240/conciert?ramdom=1",
-      name: "Korean style",
-      date: "2021-08-12",
-      description:
-        "Enjoy the best Korean dishes, with international chefs and awesome events.",
-      category: "Food Fair",
-      place: "Room A",
-      capacity: 45000,
-      assistance: 42756,
-      price: 10,
-    },
-    {
-      id: 3,
-      image: "https://loremflickr.com/320/240/conciert?ramdom=1",
-      name: "Jurassic Park",
-      date: "2021-11-02",
-      description:
-        "Let's go meet the biggest dinosaurs in the paleontology museum.",
-      category: "Museum",
-      place: "Field",
-      capacity: 82000,
-      assistance: 65892,
-      price: 15,
-    },
-    {
-      id: 4,
-      image: "https://loremflickr.com/330/240/conciert?ramdom=1",
-      name: "Parisian Museum",
-      date: "2022-11-02",
-      description:
-        "A unique tour in the city of lights, get to know one of the most iconic places.",
-      category: "Museum",
-      place: "Paris",
-      capacity: 8200,
-      estimate: 8200,
-      price: 3500,
-    },
-    {
-      id: 5,
-      image: "https://loremflickr.com/340/240/conciert?ramdom=1",
-      name: "Parisian 123",
-      date: "2022-11-02",
-      description:
-        "A unique tour in the city of lights, get to know one of the most iconic places.",
-      category: "Museum",
-      place: "Paris",
-      capacity: 8200,
-      estimate: 8200,
-      price: 3500,
-    },
-    {
-      id: 6,
-      image: "https://loremflickr.com/350/240/conciert?ramdom=1",
-      name: "Parisian 41231",
-      date: "2022-11-02",
-      description:
-        "A unique tour in the city of lights, get to know one of the most iconic places.",
-      category: "Museum",
-      place: "Paris",
-      capacity: 8200,
-      estimate: 8200,
-      price: 3500,
-    },
-    {
-      id: 7,
-      image: "https://loremflickr.com/360/240/conciert?ramdom=1",
-      name: "Parisian 41231",
-      date: "2022-11-02",
-      description:
-        "A unique tour in the city of lights, get to know one of the most iconic places.",
-      category: "Museum",
-      place: "Paris",
-      capacity: 8200,
-      estimate: 8200,
-      price: 3500,
-    },
-    {
-      id: 8,
-      image: "https://loremflickr.com/370/240/conciert?ramdom=1",
-      name: "Parisian 41231",
-      date: "2022-11-02",
-      description:
-        "A unique tour in the city of lights, get to know one of the most iconic places.",
-      category: "Museum",
-      place: "Paris",
-      capacity: 8200,
-      estimate: 8200,
-      price: 3500,
-    },
-    {
-      id: 9,
-      image: "https://loremflickr.com/380/240/conciert?ramdom=1",
-      name: "Parisian 41231",
-      date: "2022-11-02",
-      description:
-        "A unique tour in the city of lights, get to know one of the most iconic places.",
-      category: "Museum",
-      place: "Paris",
-      capacity: 8200,
-      estimate: 8200,
-      price: 3500,
-    },
+    "fechaActual": "2022-01-01",
+    "eventos": [{
+            'id': 1,
+            "image": '../assets/eventsImages/event1.jpg',
+            "name": "Collectivities Party",
+            "date": "2021-12-12",
+            "description": "Enjoy your favourite dishes, from different countries, in a unique event for the whole family.",
+            "category": "Food Fair",
+            "place": "Room A",
+            "capacity": 45000,
+            "assistance": 42756,
+            "price": 5
+        },
+        {
+            'id': 2,
+            "image": '../assets/eventsImages/event2.jpg',
+            "name": "Korean style",
+            "date": "2021-08-12",
+            "description": "Enjoy the best Korean dishes, with international chefs and awesome events.",
+            "category": "Food Fair",
+            "place": "Room A",
+            "capacity": 45000,
+            "assistance": 42756,
+            "price": 10
+        },
+        {
+            'id': 3,
+            "image": "../assets/eventsImages/event3.jpg",
+            "name": "Jurassic Park",
+            "date": "2021-11-02",
+            "description": "Let's go meet the biggest dinosaurs in the paleontology museum.",
+            "category": "Museum",
+            "place": "Field",
+            "capacity": 82000,
+            "assistance": 65892,
+            "price": 15
+        },
+        {
+            'id': 4,
+            "image": "../assets/eventsImages/event4.jpg",
+            "name": "Parisian Museum",
+            "date": "2022-11-02",
+            "description": "A unique tour in the city of lights, get to know one of the most iconic places.",
+            "category": "Museum",
+            "place": "Paris",
+            "capacity": 8200,
+            "estimate": 8200,
+            "price": 3500
+        },
+        {
+            'id': 5,
+            "image": "../assets/eventsImages/event5.jpg",
+            "name": "2022 Academy Awards",
+            "date": "2022-11-02",
+            "description": "A unique tour in the city of lights, get to know one of the most iconic places.",
+            "category": "Museum",
+            "place": "Paris",
+            "capacity": 8200,
+            "estimate": 8200,
+            "price": 3500
+        },
+        {
+            'id': 6,
+            "image": "../assets/eventsImages/event6.jpg",
+            "name": "2022 World Athletics Championships",
+            "date": "2022-11-02",
+            "description": "A unique tour in the city of lights, get to know one of the most iconic places.",
+            "category": "Museum",
+            "place": "Paris",
+            "capacity": 8200,
+            "estimate": 8200,
+            "price": 3500
+        },
+        {
+            'id': 7,
+            'image': '../assets/eventsImages/event7.jpg',
+            'name': '2023 World Athletics Championships',
+            'date': '2023-08-05',
+            'description': 'The 2023 World Athletics Championships will be held in Budapest, Hungary from August 5-13, 2023.',
+            'category': 'Sports',
+            'place': 'Budapest, Hungary',
+            'capacity': 40000,
+            'estimate': 38000,
+            'price': 150
+        },
 
-    {
-      id: 10,
-      image: "https://loremflickr.com/390/240/conciert?ramdom=1",
-      name: "Parisian 41231",
-      date: "2022-11-02",
-      description:
-        "A unique tour in the city of lights, get to know one of the most iconic places.",
-      category: "Museum",
-      place: "Paris",
-      capacity: 8200,
-      estimate: 8200,
-      price: 3500,
-    },
+        {
+            'id': 8,
+            'image': '../assets/eventsImages/event8.jpg',
+            'name': '2023 Venice Film Festival',
+            'date': '2023-09-06',
+            'description': 'The 2023 Venice Film Festival will take place from September 6-16, 2023.',
+            'category': 'Film',
+            'place': 'Venice, Italy',
+            'capacity': 1000,
+            'estimate': 950,
+            'price': 500
+        },
 
-    {
-      id: 11,
-      image: "https://loremflickr.com/400/240/conciert?ramdom=1",
-      name: "Parisian 41231",
-      date: "2022-11-02",
-      description:
-        "A unique tour in the city of lights, get to know one of the most iconic places.",
-      category: "Museum",
-      place: "Paris",
-      capacity: 8200,
-      estimate: 8200,
-      price: 3500,
-    },
+        {
+            'id': 9,
+            'image': '../assets/eventsImages/event9.jpg',
+            'name': '2023 Paris Fashion Week',
+            'date': '2023-10-01',
+            'description': 'The 2023 Paris Fashion Week will be held from October 1-9, 2023.',
+            'category': 'Fashion',
+            'place': 'Paris, France',
+            'capacity': 5000,
+            'estimate': 4800,
+            'price': 750
+        },
 
-    {
-      id: 12,
-      image: "https://loremflickr.com/401/240/conciert?ramdom=1",
-      name: "Parisian 41231",
-      date: "2022-11-02",
-      description:
-        "A unique tour in the city of lights, get to know one of the most iconic places.",
-      category: "Museum",
-      place: "Paris",
-      capacity: 8200,
-      estimate: 8200,
-      price: 3500,
-    },
+        {
+            'id': 10,
+            'image': '../assets/eventsImages/event10.jpg',
+            'name': '2023 Berlin Marathon',
+            'date': '2023-09-24',
+            'description': 'The 2023 Berlin Marathon will take place on September 24, 2023.',
+            'category': 'Sports',
+            'place': 'Berlin, Germany',
+            'capacity': 40000,
+            'estimate': 38500,
+            'price': 100
+        },
 
-    {
-      id: 13,
-      image: "https://loremflickr.com/402/240/conciert?ramdom=1",
-      name: "Parisian 41231",
-      date: "2022-11-02",
-      description:
-        "A unique tour in the city of lights, get to know one of the most iconic places.",
-      category: "Museum",
-      place: "Paris",
-      capacity: 8200,
-      estimate: 8200,
-      price: 3500,
-    },
+        {
+            'id': 11,
+            'image': '../assets/eventsImages/event11.jpg',
+            'name': '2023 Barcelona Jazz Festival',
+            'date': '2023-10-20',
+            'description': 'The 2023 Barcelona Jazz Festival will be held from October 20-30, 2023.',
+            'category': 'Music',
+            'place': 'Barcelona, Spain',
+            'capacity': 2000,
+            'estimate': 1950,
+            'price': 80
+        },
 
-    {
-      id: 14,
-      image: "https://loremflickr.com/403/240/conciert?ramdom=1",
-      name: "Parisian 41231",
-      date: "2022-11-02",
-      description:
-        "A unique tour in the city of lights, get to know one of the most iconic places.",
-      category: "Museum",
-      place: "Paris",
-      capacity: 8200,
-      estimate: 8200,
-      price: 3500,
-    },
+        {
+            'id': 12,
+            'image': '../assets/eventsImages/event12.jpg',
+            'name': '2023 Super Bowl',
+            'date': '2023-02-05',
+            'description': 'The 2023 Super Bowl will be held on February 5, 2023 in Phoenix, Arizona.',
+            'category': 'Sports',
+            'place': 'Phoenix, Arizona, USA',
+            'capacity': 70000,
+            'estimate': 68500,
+            'price': 500
+        } ,
+        {
+            'id': 13,
+            'image': '../assets/eventsImages/event13.jpg',
+            'name': '2023 Academy Awards',
+            'date': '2023-03-26',
+            'description': 'The 95th Academy Awards ceremony will be held on March 26, 2023 at the Dolby Theatre in Hollywood, Los Angeles, California.',
+            'category': 'Film',
+            'place': 'Los Angeles, California',
+            'capacity': 3400,
+            'estimate': 3300,
+            'price': 500
+        },
+        {
+            'id': 14,
+            'image': '../assets/eventsImages/event14.jpg',
+            'name': '2023 Wimbledon Championships',
+            'date': '2023-06-26',
+            'description': 'The 2023 Wimbledon Championships will be held from June 26 to July 9, 2023 at the All England Lawn Tennis and Croquet Club in London, England.',
+            'category': 'Sports',
+            'place': 'London, England',
+            'capacity': 42000,
+            'estimate': 41000,
+            'price': 300
+        },
 
-    {
-      id: 15,
-      image: "https://loremflickr.com/404/240/conciert?ramdom=1",
-      name: "Parisian 41231",
-      date: "2022-11-02",
-      description:
-        "A unique tour in the city of lights, get to know one of the most iconic places.",
-      category: "Museum",
-      place: "Paris",
-      capacity: 8200,
-      estimate: 8200,
-      price: 3500,
-    },
+        {
+            'id': 15,
+            'image': '../assets/eventsImages/event15.jpg',
+            'name': '2023 Edinburgh Fringe Festival',
+            'date': '2023-08-04',
+            'description': 'The 2023 Edinburgh Fringe Festival will be held from August 4-28, 2023 in Edinburgh, Scotland.',
+            'category': 'Theater',
+            'place': 'Edinburgh, Scotland',
+            'capacity': 2000,
+            'estimate': 1900,
+            'price': 50
+        },
 
-    {
-      id: 16,
-      image: "https://loremflickr.com/405/240/conciert?ramdom=1",
-      name: "Parisian 41231",
-      date: "2022-11-02",
-      description:
-        "A unique tour in the city of lights, get to know one of the most iconic places.",
-      category: "Museum",
-      place: "Paris",
-      capacity: 8200,
-      estimate: 8200,
-      price: 3500,
-    },
-  ],
-};
+        {
+            'id': 16,
+            'image': '../assets/eventsImages/event16.jpg',
+            'name': '2023 Oktoberfest',
+            'date': '2023-09-16',
+            'description': 'The 2023 Oktoberfest will be held from September 16 to October 3, 2023 in Munich, Germany.',
+            'category': 'Festival',
+            'place': 'Munich, Germany',
+            'capacity': 60000,
+            'estimate': 57000,
+            'price': 20
+        }
+
+    ]
+}
 
 function newCard(evento) {
   let divForCard = document.createElement("div");
@@ -245,7 +229,7 @@ function tarjetasHome() {
           </div>
           `;
 
-  for (let i = 0; i < 4; i++) {
+  for (let i = 0; i < 16; i++) {
     let card = newCard(ArrayData.eventos[i]);
 
     divCarouselActive.querySelector("#card_template").appendChild(card);
