e3f5d33d2d1b33c50dbc96309907a4f9a1795408
ROBERTG96 <71914244+ROBERTG96@users.noreply.github.com>
2023-03-23 15:10:44 -0500

    TemplateCardHome

--- src/lib/Js-page1.js
+++ src/lib/Js-page1.js
@@ -1,246 +1,275 @@
-const ArrayData = [{
-    "fechaActual": "2022-01-01",
-    "eventos": [
-        {
-            'id': 1,
-            "image": 'https://loremflickr.com/300/240/conciert?ramdom=1',
-            "name": "Collectivities Party",
-            "date": "2021-12-12",
-            "description": "Enjoy your favourite dishes, from different countries, in a unique event for the whole family.",
-            "category": "Food Fair",
-            "place": "Room A",
-            "capacity": 45000,
-            "assistance": 42756,
-            "price": 5
-        },
-        {
-            'id': 2,
-            "image": 'https://loremflickr.com/310/240/conciert?ramdom=1',
-            "name": "Korean style",
-            "date": "2021-08-12",
-            "description": "Enjoy the best Korean dishes, with international chefs and awesome events.",
-            "category": "Food Fair",
-            "place": "Room A",
-            "capacity": 45000,
-            "assistance": 42756,
-            "price": 10
-        },
-        {
-            'id': 3,
-            "image": "https://loremflickr.com/320/240/conciert?ramdom=1",
-            "name": "Jurassic Park",
-            "date": "2021-11-02",
-            "description": "Let's go meet the biggest dinosaurs in the paleontology museum.",
-            "category": "Museum",
-            "place": "Field",
-            "capacity": 82000,
-            "assistance": 65892,
-            "price": 15
-        },
-        {
-            'id': 4,
-            "image": "https://loremflickr.com/330/240/conciert?ramdom=1",
-            "name": "Parisian Museum",
-            "date": "2022-11-02",
-            "description": "A unique tour in the city of lights, get to know one of the most iconic places.",
-            "category": "Museum",
-            "place": "Paris",
-            "capacity": 8200,
-            "estimate": 8200,
-            "price": 3500
-        },
-        {
-            'id': 5,
-            "image": "https://loremflickr.com/340/240/conciert?ramdom=1",
-            "name": "Parisian 123",
-            "date": "2022-11-02",
-            "description": "A unique tour in the city of lights, get to know one of the most iconic places.",
-            "category": "Museum",
-            "place": "Paris",
-            "capacity": 8200,
-            "estimate": 8200,
-            "price": 3500
-        },
-        {
-            'id': 6,
-            "image": "https://loremflickr.com/350/240/conciert?ramdom=1",
-            "name": "Parisian 41231",
-            "date": "2022-11-02",
-            "description": "A unique tour in the city of lights, get to know one of the most iconic places.",
-            "category": "Museum",
-            "place": "Paris",
-            "capacity": 8200,
-            "estimate": 8200,
-            "price": 3500
-        },
-        {
-            'id': 7,
-            "image": "https://loremflickr.com/360/240/conciert?ramdom=1",
-            "name": "Parisian 41231",
-            "date": "2022-11-02",
-            "description": "A unique tour in the city of lights, get to know one of the most iconic places.",
-            "category": "Museum",
-            "place": "Paris",
-            "capacity": 8200,
-            "estimate": 8200,
-            "price": 3500
-        }
-        ,
-        {
-            'id': 8,
-            "image": "https://loremflickr.com/370/240/conciert?ramdom=1",
-            "name": "Parisian 41231",
-            "date": "2022-11-02",
-            "description": "A unique tour in the city of lights, get to know one of the most iconic places.",
-            "category": "Museum",
-            "place": "Paris",
-            "capacity": 8200,
-            "estimate": 8200,
-            "price": 3500
-        },
-        {
-            'id': 9,
-            "image": "https://loremflickr.com/380/240/conciert?ramdom=1",
-            "name": "Parisian 41231",
-            "date": "2022-11-02",
-            "description": "A unique tour in the city of lights, get to know one of the most iconic places.",
-            "category": "Museum",
-            "place": "Paris",
-            "capacity": 8200,
-            "estimate": 8200,
-            "price": 3500
-        },
-        
-        {
-            'id': 10,
-            "image": "https://loremflickr.com/390/240/conciert?ramdom=1",
-            "name": "Parisian 41231",
-            "date": "2022-11-02",
-            "description": "A unique tour in the city of lights, get to know one of the most iconic places.",
-            "category": "Museum",
-            "place": "Paris",
-            "capacity": 8200,
-            "estimate": 8200,
-            "price": 3500
-        },
-        
-        {
-            'id': 11,
-            "image": "https://loremflickr.com/400/240/conciert?ramdom=1",
-            "name": "Parisian 41231",
-            "date": "2022-11-02",
-            "description": "A unique tour in the city of lights, get to know one of the most iconic places.",
-            "category": "Museum",
-            "place": "Paris",
-            "capacity": 8200,
-            "estimate": 8200,
-            "price": 3500
-        },
-        
-        {
-            'id': 12,
-            "image": "https://loremflickr.com/401/240/conciert?ramdom=1",
-            "name": "Parisian 41231",
-            "date": "2022-11-02",
-            "description": "A unique tour in the city of lights, get to know one of the most iconic places.",
-            "category": "Museum",
-            "place": "Paris",
-            "capacity": 8200,
-            "estimate": 8200,
-            "price": 3500
-        },
+const ArrayData = {
+  fechaActual: "2022-01-01",
+  eventos: [
+    {
+      id: 1,
+      image: "https://loremflickr.com/300/240/conciert?ramdom=1",
+      name: "Collectivities Party",
+      date: "2021-12-12",
+      description:
+        "Enjoy your favourite dishes, from different countries, in a unique event for the whole family.",
+      category: "Food Fair",
+      place: "Room A",
+      capacity: 45000,
+      assistance: 42756,
+      price: 5,
+    },
+    {
+      id: 2,
+      image: "https://loremflickr.com/310/240/conciert?ramdom=1",
+      name: "Korean style",
+      date: "2021-08-12",
+      description:
+        "Enjoy the best Korean dishes, with international chefs and awesome events.",
+      category: "Food Fair",
+      place: "Room A",
+      capacity: 45000,
+      assistance: 42756,
+      price: 10,
+    },
+    {
+      id: 3,
+      image: "https://loremflickr.com/320/240/conciert?ramdom=1",
+      name: "Jurassic Park",
+      date: "2021-11-02",
+      description:
+        "Let's go meet the biggest dinosaurs in the paleontology museum.",
+      category: "Museum",
+      place: "Field",
+      capacity: 82000,
+      assistance: 65892,
+      price: 15,
+    },
+    {
+      id: 4,
+      image: "https://loremflickr.com/330/240/conciert?ramdom=1",
+      name: "Parisian Museum",
+      date: "2022-11-02",
+      description:
+        "A unique tour in the city of lights, get to know one of the most iconic places.",
+      category: "Museum",
+      place: "Paris",
+      capacity: 8200,
+      estimate: 8200,
+      price: 3500,
+    },
+    {
+      id: 5,
+      image: "https://loremflickr.com/340/240/conciert?ramdom=1",
+      name: "Parisian 123",
+      date: "2022-11-02",
+      description:
+        "A unique tour in the city of lights, get to know one of the most iconic places.",
+      category: "Museum",
+      place: "Paris",
+      capacity: 8200,
+      estimate: 8200,
+      price: 3500,
+    },
+    {
+      id: 6,
+      image: "https://loremflickr.com/350/240/conciert?ramdom=1",
+      name: "Parisian 41231",
+      date: "2022-11-02",
+      description:
+        "A unique tour in the city of lights, get to know one of the most iconic places.",
+      category: "Museum",
+      place: "Paris",
+      capacity: 8200,
+      estimate: 8200,
+      price: 3500,
+    },
+    {
+      id: 7,
+      image: "https://loremflickr.com/360/240/conciert?ramdom=1",
+      name: "Parisian 41231",
+      date: "2022-11-02",
+      description:
+        "A unique tour in the city of lights, get to know one of the most iconic places.",
+      category: "Museum",
+      place: "Paris",
+      capacity: 8200,
+      estimate: 8200,
+      price: 3500,
+    },
+    {
+      id: 8,
+      image: "https://loremflickr.com/370/240/conciert?ramdom=1",
+      name: "Parisian 41231",
+      date: "2022-11-02",
+      description:
+        "A unique tour in the city of lights, get to know one of the most iconic places.",
+      category: "Museum",
+      place: "Paris",
+      capacity: 8200,
+      estimate: 8200,
+      price: 3500,
+    },
+    {
+      id: 9,
+      image: "https://loremflickr.com/380/240/conciert?ramdom=1",
+      name: "Parisian 41231",
+      date: "2022-11-02",
+      description:
+        "A unique tour in the city of lights, get to know one of the most iconic places.",
+      category: "Museum",
+      place: "Paris",
+      capacity: 8200,
+      estimate: 8200,
+      price: 3500,
+    },
 
-        {
-            'id': 13,
-            "image": "https://loremflickr.com/402/240/conciert?ramdom=1",
-            "name": "Parisian 41231",
-            "date": "2022-11-02",
-            "description": "A unique tour in the city of lights, get to know one of the most iconic places.",
-            "category": "Museum",
-            "place": "Paris",
-            "capacity": 8200,
-            "estimate": 8200,
-            "price": 3500
-        },
+    {
+      id: 10,
+      image: "https://loremflickr.com/390/240/conciert?ramdom=1",
+      name: "Parisian 41231",
+      date: "2022-11-02",
+      description:
+        "A unique tour in the city of lights, get to know one of the most iconic places.",
+      category: "Museum",
+      place: "Paris",
+      capacity: 8200,
+      estimate: 8200,
+      price: 3500,
+    },
 
-        {
-            'id': 14,
-            "image": "https://loremflickr.com/403/240/conciert?ramdom=1",
-            "name": "Parisian 41231",
-            "date": "2022-11-02",
-            "description": "A unique tour in the city of lights, get to know one of the most iconic places.",
-            "category": "Museum",
-            "place": "Paris",
-            "capacity": 8200,
-            "estimate": 8200,
-            "price": 3500
-        },
+    {
+      id: 11,
+      image: "https://loremflickr.com/400/240/conciert?ramdom=1",
+      name: "Parisian 41231",
+      date: "2022-11-02",
+      description:
+        "A unique tour in the city of lights, get to know one of the most iconic places.",
+      category: "Museum",
+      place: "Paris",
+      capacity: 8200,
+      estimate: 8200,
+      price: 3500,
+    },
 
-        {
-            'id': 15,
-            "image": "https://loremflickr.com/404/240/conciert?ramdom=1",
-            "name": "Parisian 41231",
-            "date": "2022-11-02",
-            "description": "A unique tour in the city of lights, get to know one of the most iconic places.",
-            "category": "Museum",
-            "place": "Paris",
-            "capacity": 8200,
-            "estimate": 8200,
-            "price": 3500
-        },
+    {
+      id: 12,
+      image: "https://loremflickr.com/401/240/conciert?ramdom=1",
+      name: "Parisian 41231",
+      date: "2022-11-02",
+      description:
+        "A unique tour in the city of lights, get to know one of the most iconic places.",
+      category: "Museum",
+      place: "Paris",
+      capacity: 8200,
+      estimate: 8200,
+      price: 3500,
+    },
 
-        {
-            'id': 16,
-            "image": "https://loremflickr.com/405/240/conciert?ramdom=1",
-            "name": "Parisian 41231",
-            "date": "2022-11-02",
-            "description": "A unique tour in the city of lights, get to know one of the most iconic places.",
-            "category": "Museum",
-            "place": "Paris",
-            "capacity": 8200,
-            "estimate": 8200,
-            "price": 3500
-        }
-    ]
-}]
+    {
+      id: 13,
+      image: "https://loremflickr.com/402/240/conciert?ramdom=1",
+      name: "Parisian 41231",
+      date: "2022-11-02",
+      description:
+        "A unique tour in the city of lights, get to know one of the most iconic places.",
+      category: "Museum",
+      place: "Paris",
+      capacity: 8200,
+      estimate: 8200,
+      price: 3500,
+    },
+
+    {
+      id: 14,
+      image: "https://loremflickr.com/403/240/conciert?ramdom=1",
+      name: "Parisian 41231",
+      date: "2022-11-02",
+      description:
+        "A unique tour in the city of lights, get to know one of the most iconic places.",
+      category: "Museum",
+      place: "Paris",
+      capacity: 8200,
+      estimate: 8200,
+      price: 3500,
+    },
+
+    {
+      id: 15,
+      image: "https://loremflickr.com/404/240/conciert?ramdom=1",
+      name: "Parisian 41231",
+      date: "2022-11-02",
+      description:
+        "A unique tour in the city of lights, get to know one of the most iconic places.",
+      category: "Museum",
+      place: "Paris",
+      capacity: 8200,
+      estimate: 8200,
+      price: 3500,
+    },
+
+    {
+      id: 16,
+      image: "https://loremflickr.com/405/240/conciert?ramdom=1",
+      name: "Parisian 41231",
+      date: "2022-11-02",
+      description:
+        "A unique tour in the city of lights, get to know one of the most iconic places.",
+      category: "Museum",
+      place: "Paris",
+      capacity: 8200,
+      estimate: 8200,
+      price: 3500,
+    },
+  ],
+};
 
 function newCard(evento) {
-    
-    let divForCard = document.createElement('div');
-    divForCard.className = 'col-lg-3 col-sm-6';
-    divForCard.innerHTML = `
-    <div class="card" >
+  let divForCard = document.createElement("div");
+  divForCard.className = "col-lg-3 col-sm-6";
+  divForCard.innerHTML = `
+    <div class="card">
     <img src="${evento.image}" class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">Title</h5>
-      <p class="card-text">Description text</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+      <h5 class="card-title">${evento.name}</h5>
+      <p class="card-text">${evento.description}</p>
+      <a href="./details.html" onclick="detalleCard(${evento.id})" class="btn btn-primary">Go somewhere</a>
     </div>
   </div>
-    `
+    `;
+
+  return divForCard;
+}
+
+function tarjetasHome() {
+  let divCarouselActive = document.createElement("div");
+  divCarouselActive.className = "carousel-item active";
+  divCarouselActive.innerHTML = `<div class="container">
+            <div class="row" id="card_template"> 
+            </div>
+          </div>
+          `;
+
+  for (let i = 0; i < 4; i++) {
+    let card = newCard(ArrayData.eventos[i]);
+
+    divCarouselActive.querySelector("#card_template").appendChild(card);
+  }
+
+  document.getElementById("TemplateCardHome").appendChild(divCarouselActive)
 
 }
 
-function Carousel(eventos) {
+tarjetasHome();
+console.log(newCard(ArrayData.eventos[1]));
 
-    console.log(eventos);
-
-}
-
-console.log(Carousel(ArrayData));
-
-const details_card = document.querySelector('details_card');
+const details_card = document.querySelector("details_card");
 
 function detalleCard(id) {
-
-    ArrayData.forEach(data => {
-        for (let i = 0; i < data.eventos.length; i++) {
-            if (data.eventos[i].id === parseInt(id)) {
-                localStorage.clear();
-                return localStorage.setItem("detail_temp", JSON.stringify(data.eventos[i]));
-            }
-        }
-
-    })
-
+  ArrayData.forEach((data) => {
+    for (let i = 0; i < data.eventos.length; i++) {
+      if (data.eventos[i].id === parseInt(id)) {
+        localStorage.clear();
+        return localStorage.setItem(
+          "detail_temp",
+          JSON.stringify(data.eventos[i])
+        );
+      }
+    }
+  });
 }
