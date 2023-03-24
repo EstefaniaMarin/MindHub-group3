3a2cdee980e26c9112521a4f82457a759d7ecc89
ROBERTG96 <71914244+ROBERTG96@users.noreply.github.com>
2023-03-24 02:21:56 -0500

    Changed All Design - Project In 90% complete
    Changed All Design - Project In 90% complete
    - Pendiente upcoming events
    - Pendiente past events
    - Pendiente cambiar imagenes carrusel primario

--- src/lib/Js-page2.js
+++ src/lib/Js-page2.js
@@ -8,21 +8,28 @@ function CargarDetails() {
     let detail = JSON.parse(detailsCard);
 
     CardImg.innerHTML = `
-
     <div class='card_details' >
     <img src="${detail.image}" class='card_details' alt="${detail.name}"></img>
     </div>
     `
-
     details_card.innerHTML = `
      <h3 class="card-title">Category: ${detail.category}</h3>
          <h2 class="card-title">${detail.name}</h2>  
          <p class="card-text"> ${detail.description}</p>
-         <p class="card-text"><i class="bi bi-building-fill-up icon_detail"></i> ${detail.place}</p>
-         <p class="card-text"><i class="bi bi-people-fill icon_detail"></i> ${detail.capacity}</p>
-
-     <h4 class="card-title mb-3"><strong><i class="bi bi-currency-exchange icon_detail"></i> ${detail.price} $</strong></h4>
-                `
+         <div class="card-body">
+         <ul class="list-group list-group-flush">
+         <p class="list-group-item" data-toggle="tooltip" data-placement="bottom" title="Date"><i class="bi bi-calendar-event-fill icon_detail"></i> ${detail.date}</p>
+         <p class="list-group-item" data-toggle="tooltip" data-placement="bottom" title="Category"><i class="bi bi-bookmark-star-fill icon_detail"></i> ${detail.category}</p>
+         <p class="list-group-item" data-toggle="tooltip" data-placement="bottom" title="Capacity"><i class="bi bi-people-fill icon_detail"></i> ${detail.capacity}</p>
+         <p class="list-group-item" data-toggle="tooltip" data-placement="bottom" title="Place"><i class="bi bi-geo-alt-fill icon_detail"></i> ${detail.place}</p>
+         <p class="list-group-item" data-toggle="tooltip" data-placement="bottom" title="Price"><strong><i class="bi bi-currency-exchange icon_detail"></i> ${detail.price}</strong></p>
+         </ul>
+         <div class="card-body">
+         <a class="btn background-color text-light w-100 "  data-toggle="tooltip" data-placement="bottom" title="Contact For - ${detail.name}" href="./contact.html" target='_blank'"> <i class="bi bi-send-check-fill icon_view"></i></a>
+         </div>
+     `
 }
 
-console.log( CargarDetails());
+
+
+console.log(CargarDetails());
